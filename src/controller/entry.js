3794bc4df94946e0d09525ed82c1ccd1eab769a3
mathias akari <cyphercode59@gmail.com>
2023-11-22 11:32:00 +0100

    new

--- src/controller/entry.js
+++ src/controller/entry.js
@@ -1,10 +1,7 @@
-
-const { pipeline, Transform } = require('stream');
-const csvParser = require('csv-parser');
-const MSISDNTransform = require('../utils/MSISDNTransform.js');
+const { pipeline, Transform, Readable } = require("stream");
+const csvParser = require("csv-parser");
+const MSISDNTransform = require("../utils/MSISDNTransform.js");
 const Entry = require("../model/entry.js");
-
-
 
 function dbStream() {
   return new Transform({
@@ -20,31 +17,78 @@ function dbStream() {
         await entry.save();
         callback();
       } catch (error) {
-        console.error('Error inserting into the database:', error);
+        console.error("Error inserting into the database:", error);
         callback(error);
       }
     },
   });
 }
 
+// Function to normalize MSISDN to international format
+function normalizeMSISDN(msisdn) {
+  // Example: Assuming all MSISDNs start with '0' and you want to replace it with '+234'
+  return `+234${msisdn.slice(1)}`;
+}
 
- const handleFileUpload = async (req,res)=> {
-    const { file } = req;
-    // Use pipeline to handle streaming from request to database
-    pipeline(
-      file.buffer,
-      csvParser({ headers: ['MSISDN', 'quantity', 'narration'] }),
-      new MSISDNTransform(),
-      dbStream(),
-      (err) => {
-        if (err) {
-          console.error('Pipeline failed:', err);
-          res.status(500).send('Internal Server Error');
-        } else {
-          res.send('Upload successful');
-        }
-      }
-    );
+
+const csvTransform = csvParser({
+  mapHeaders: ({ header, index }) => header.trim(),
+});
+
+const transformStream = new Transform({
+  objectMode: true,
+  transform(chunk, encoding, callback) {
+    const msisdn = normalizeMSISDN(chunk.MSISDN);
+
+    this.push({
+      msisdn,
+      quantity: parseInt(chunk.quantity),
+      narration: chunk.narration,
+    });
+    callback();
+  },
+});
+// Function to process CSV file
+async function processCSV(buffer) {
+  // Signal the end of the stream
+
+  const entries = [];
+
+
+}
+
+const handleFileUpload = async (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
   }
 
-  module.exports = handleFileUpload
+  // Determine the correct buffer source based on multer configuration
+  const fileBuffer = req.file.buffer;
+  const readableStream = new Readable();
+  readableStream.push(fileBuffer);
+  readableStream.push(null);
+  if (!fileBuffer) {
+    return res.status(500).send("Invalid file buffer.");
+  }
+  // Use pipeline to handle streaming from request to database
+  console.log("file", fileBuffer);
+  pipeline(
+    readableStream,
+    csvTransform,
+    transformStream,
+    dbStream(),
+    (err) => {
+      if (err) {
+        console.error("Pipeline failed:", err);
+        res.status(500).send("Internal Server Error");
+      } else {
+        res.send("Upload successful");
+      }
+    }
+  );
+};
+
+
+
+
+module.exports = handleFileUpload;
